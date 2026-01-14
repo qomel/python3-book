953ed88c4ab327478a2c274c4e925295c4002943
Dominik Pazurek <80347310+qomel@users.noreply.github.com>
2026-01-14 20:00:21 +0100

    Update first_captcher.py

--- first_captcher.py
+++ first_captcher.py
@@ -118,8 +118,121 @@ def cars_py5():
             print(car.upper())
         else:
             print(car.title())
-    
-    
-cars_py5()
 
- 
+def five_1():
+    cars = ['audi',
+            'bmw',
+            'honda',
+            'kiv',
+            'reanulte',
+            'porsche'
+            ]
+    
+    there_is = False
+    
+    print("Czy w cars jest BMW?")   
+    if 'bmw' in cars:
+        there_is = True
+        print(there_is)
+    else:
+        print(there_is)
+        
+    there_is = False
+    if 'preugote' not in cars:
+        print(there_is)
+    else:
+        there_is = True
+        print(there_is)    
+
+def five_3():
+    alien_color = 'zielony'
+    
+    if 'zielony' in alien_color:
+        print('5pkt')
+
+    if alien_color == 'zielony':
+        print('5pkt')
+    else:
+        print('10pkt')
+    
+    if alien_color == 'zielony':
+        print('5pkt')
+    elif alien_color == 'zółty':
+        print('10pkt')
+    elif alien_color == 'czerwony':
+        print('15pkt')
+    
+    
+    # ----- 5.6
+    age = 66
+    
+    if age < 2:
+        print('Niemowle')
+    elif 4 > age <= 2:
+        print('uczy się chodzicić')
+    elif 20 > age <= 13:
+        print('Nastolatek')
+    elif 65 > age <= 20:
+        print('Dorosły')
+    elif age > 65:
+        print('senior')
+        
+    fruits = ['banan',
+              'jablko',
+              'truskawka',
+              'brzoskwinia'
+              ]
+    
+    if 'banan' in fruits:
+        print('Lubisz banana')
+        
+    
+def five_8():
+    users = [
+             'admin',
+             'dominik',
+             'michalina',
+             'kamil',
+             'mateusz'
+             ]
+    
+    new_users = ['dominik',
+                'adam',
+                'ania',
+                'DOMINIK'
+                ]
+    
+    if not users:
+        print('Nie ma userow')
+    
+    for user in users:  
+        if user == 'admin':
+            print(f'Witaj {user.title()}, chcesz przejrzeć raport?')
+        else:
+            print(f'Witaj {user.title()}')
+    
+    # ---- 5.10
+    for n_user in new_users:
+        if n_user.lower() in users:
+            print(f'Ta nazwa jest zajęta {n_user}')
+        else:
+            print(f'Wolna nazwa {n_user.title()}')
+    
+    # ---- 5.11
+    numbers = [1, 2, 3,
+               4, 5, 6,
+               7, 8, 9
+               ]
+    
+    for number in numbers:
+        if number == 1:
+            print(f"{number}st")
+        elif number == 2:
+            print(f"{number}st")
+        elif number == 3:
+            print(f"{number}st")
+        else:
+            print(f'{number}th')
+    
+    
+five_8()
